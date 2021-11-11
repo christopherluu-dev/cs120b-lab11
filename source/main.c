5e94e143b045adf6ed43fbccb5ade737ccb221ad
Chris <christopherluu.dev@gmail.com>
2021-11-11 14:51:47 -0800

    added the header files

--- source/main.c
+++ source/main.c
@@ -1,13 +1,20 @@
 /*	Author: christopherluu
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 22
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo Video:
  */
 #include <avr/io.h>
+#include "io.h"
+#include "timer.h"
+#include "keypad.h"
+#include "scheduler.h"
+
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
